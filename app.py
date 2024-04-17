e6045e5cbe9c5621f2d98da08dd8621e1c077091
Thant Sin Oo ( Howard Chang ) <drawoh@passion.xyz>
2024-04-17 19:55:20 +0800

    Add more log

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ class InferlessPythonModel:
         )
         script_path = os.path.join(__location__, "custom_nodes_setup.sh")
         print(f"Running {script_path}")
+        result = subprocess.run([f"ls {__location__}"], capture_output=True, text=True)
+        print(result)
+
+        result = subprocess.run([f"ls {script_path}"], capture_output=True, text=True)
+        print(result)
+
         result = subprocess.run([f"cat {script_path}"], capture_output=True, text=True)
         print(result)
 
