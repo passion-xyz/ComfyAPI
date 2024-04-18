73c1d037f7fd2f941b063b6e8af7803b9241a70e
Thant Sin Oo ( Howard Chang ) <drawoh@passion.xyz>
2024-04-18 20:09:29 +0800

    Add app.py

--- app.py
+++ app.py
@@ -3,8 +3,47 @@ import os
 import base64
 import sys
 import subprocess
+from PIL import Image
+import re
+import json
+from urllib import request
+import requests
 
 class InferlessPythonModel:
+
+    @staticmethod
+    def convert_image_to_base64(image_path):
+        with Image.open(image_path) as image:
+            buffered = BytesIO()
+            image.save(buffered, format="PNG")
+            return base64.b64encode(buffered.getvalue()).decode()
+
+    @staticmethod
+    def process_single_image(image_path):
+        try:
+            base64_image = InferlessPythonModel.convert_image_to_base64(image_path)
+            # os.remove(image_path)  # Delete the image after conversion
+            return base64_image
+        except Exception as e:
+            print(f"Error processing {image_path}: {e}")
+            return None
+        
+    @staticmethod
+    def get_final_image_name(directory):
+        pattern = re.compile(r"ComfyUI_(\d+)_\.png$")
+        max_number = 0
+
+        print("*******************************", flush=True)
+        for filename in os.listdir(directory):
+            print("FileName: ", filename, flush=True)
+            match = pattern.match(filename)
+            if match:
+                number = int(match.group(1))
+                max_number = max(max_number, number)
+
+        print(f"Max number: {max_number}", flush=True)
+        return f"ComfyUI_{max_number:05d}_.png"
+
     def initialize(self):
         import subprocess
 
@@ -13,18 +52,39 @@ class InferlessPythonModel:
         )
         file_name = os.path.join(__location__, "main.py")
         self.process = subprocess.Popen(["python3.10", file_name])
-        mounted_path = '/var/nfs-mount/Passion-ComfyUI-Volumes'
-        sys.path.insert(1, mounted_path)
-        try:
-            ls_output = subprocess.check_output(['ls', mounted_path])
-            print("Contents of the mounted path:", ls_output.decode())
-        except subprocess.CalledProcessError as e:
-            print("Failed to list contents of the mounted path:", e)
         
     def infer(self, inputs):
-        print(inputs)
-        # prompt = inputs["prompt"]
-        return { "generated_image_base64" : 'prompt' }
+        try:
+            print("Infer Started", flush=True)
+            prompt = inputs["prompt"]
+
+            print("Recieved prompt:", prompt, flush=True)
+
+            p = {"prompt": prompt}
+
+            data = json.dumps(p).encode("utf-8")
+            print("Prompt Encoding Happened", flush=True)
+
+            req = request.Request("http://127.0.0.1:8188/prompt", data=data)
+            request.urlopen(req)
+
+            task_completed = False
+            while task_completed != True:
+                response = requests.get("http://127.0.0.1:8188/queue")
+                if response.json()["queue_running"] == []:
+                    task_completed = True
+
+            print("Queue Completed", flush=True)
+            final_image_name = InferlessPythonModel.get_final_image_name(
+                "/var/nfs-mount/Passion-ComfyUI-Volumes/output"
+            )
+            image_path = f"/var/nfs-mount/Passion-ComfyUI-Volumes/output/{final_image_name}"
+            base64_image = InferlessPythonModel.process_single_image(image_path)
+
+            return {"generated_image": base64_image}
+        except Exception as e:
+            print(f"Error processing: {e}", flush=True)
+            return None
         
     def finalize(self):
         self.pipe = None
