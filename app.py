e298097e6aaf958ee19452a29f88a585473e7721
Thant Sin Oo ( Howard Chang ) <drawoh@passion.xyz>
2024-04-20 03:53:04 +0800

    Redo

--- app.py
+++ app.py
@@ -1,34 +1,13 @@
-import sys
-import os
-
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__))
-)
-
-OUTPUT_DIR = "/var/nfs-mount/Passion-ComfyUI-Volumes/output"
-INPUT_DIR = "/var/nfs-mount/Passion-ComfyUI-Volumes/input"
-HELPER_DIR = "/var/nfs-mount/Passion-ComfyUI-Volumes/helpers"
-COMFYUI_TEMP_OUTPUT_DIR = "/tmp"
-
-sys.path.insert(1, os.path.join(__location__, "ComfyUI"))
-sys.path.insert(1, os.path.join(__location__, "ComfyUI/comfy"))
-sys.path.insert(1, HELPER_DIR)
-
-from io import BytesIO
-import base64
-import subprocess
-from PIL import Image
-import re
 import json
-from urllib import request
+from urllib import request, parse
 import requests
-
-print("OUTPUT_DIR: ", OUTPUT_DIR)
-print("INPUT_DIR: ", INPUT_DIR)
-print("HELPER_DIR: ", HELPER_DIR)
-print("COMFYUI_TEMP_OUTPUT_DIR: ", COMFYUI_TEMP_OUTPUT_DIR)
-
-# from helpers.comfyui import ComfyUI
+from tqdm import tqdm
+import os
+import asyncio
+import base64
+from PIL import Image
+from io import BytesIO
+import re
 
 class InferlessPythonModel:
     @staticmethod
@@ -46,8 +25,8 @@ class InferlessPythonModel:
             return base64_image
         except Exception as e:
             print(f"Error processing {image_path}: {e}")
-            return e
-        
+            return None
+
     @staticmethod
     def get_final_image_name(directory):
         pattern = re.compile(r"ComfyUI_(\d+)_\.png$")
@@ -65,20 +44,35 @@ class InferlessPythonModel:
         return f"ComfyUI_{max_number:05d}_.png"
 
     def initialize(self):
-        file_name = os.path.join(__location__, "./ComfyUI/main.py")
-        self.process = subprocess.Popen(["python3.10", file_name, "--highvram"])
-        # self.process = subprocess.Popen(["python3.10", file_name, "--listen 0.0.0.0 --normalvram --disable-smart-memory"])
-        # self.comfyUI = ComfyUI("127.0.0.1:8188")
-        # self.comfyUI.start_server(OUTPUT_DIR, INPUT_DIR)
-        
+        import subprocess
+
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__))
+        )
+        file_name = os.path.join(__location__, "main.py")
+        self.process = subprocess.Popen(["python3.10", file_name])
+
+
     def infer(self, inputs):
         try:
             print("Infer Started", flush=True)
-            workflow = inputs["workflow"]
+            positive_token = inputs["positive_token"]
+            negative_token = inputs["negative_token"]
+            workflow_file_name = f"{inputs["workflow"]}.json"
 
-            print("Recieved workflow:", workflow, flush=True)
+            params = json.loads(inputs["parameters"])
+            __location__ = os.path.realpath(
+                os.path.join(os.getcwd(), os.path.dirname(__file__))
+            )
+            print("Recieved parameters:", params, flush=True)
 
-            p = {"prompt": workflow}
+            prompt = json.loads(
+                open(f"{__location__}/workflows/{workflow_file_name}").read()
+                .replace("$$POSITIVE_TOKEN$$", positive_token)
+                .replace("$$NEGATIVE_TOKEN$$", negative_token)
+            )
+            
+            p = {"prompt": prompt}
 
             data = json.dumps(p).encode("utf-8")
             print("Prompt Encoding Happened", flush=True)
@@ -102,17 +96,15 @@ class InferlessPythonModel:
             return {"generated_image": base64_image}
         except Exception as e:
             print(f"Error processing: {e}", flush=True)
-            return e
-        
-    def finalize(self):
+            return None
+
+    def finalize(self, args):
         print("Finalizing", flush=True)
-        # self.pipe = None
-        # self.process.terminate()
-
-        self.comfyUI.clear_queue()
-        for directory in [OUTPUT_DIR, INPUT_DIR, COMFYUI_TEMP_OUTPUT_DIR]:
-            if os.path.exists(directory):
-                shutil.rmtree(directory)
-            os.makedirs(directory)
+        self.process.terminate()
 
 
+# if __name__ == "__main__":
+#     model = InferlessPythonModel()
+#     model.initialize()
+#     ab = model.infer({"workflow": "txt_2_img", "parameters": {"prompt": "masterpiece image of a smart dog wearing a coat and tie and glasses"}})
+#     model.finalize()
--- input_schema.py
+++ input_schema.py
@@ -1,5 +1,17 @@
 INPUT_SCHEMA = {
-    "workflow": {
+    "workflow_file_name": {
+        'datatype': 'BYTES',
+        'required': True,
+        'shape': [1],
+        'example': ["txt_2_img.json"]
+    },
+    "positive_token": {
+        'datatype': 'BYTES',
+        'required': True,
+        'shape': [1],
+        'example': ["There is a fine house in the forest"]
+    },
+    "negative_token": {
         'datatype': 'BYTES',
         'required': True,
         'shape': [1],
