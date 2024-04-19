028de1bf9caa4b87215e920e65baeb1c693e9d68
Thant Sin Oo ( Howard Chang ) <drawoh@passion.xyz>
2024-04-20 06:23:19 +0800

    Try to log error

--- app.py
+++ app.py
@@ -55,10 +55,10 @@ class InferlessPythonModel:
     def initialize(self):
         file_name = os.path.join(__location__, "main.py")
         print(f"Initializing {file_name}", flush=True)
-
-        self.process = Popen(["python3.10", "main.py"], stdout=PIPE, stderr=STDOUT)
-        with self.process.stdout:
+        self.process = Popen(["python3.10", "main.py"], stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True)
+        with self.process.stdout, self.process.stderr:
             log_subprocess_output(self.process.stdout)
+            log_subprocess_output(self.process.stderr)
         print(f"Initialization Complete - Server Running {self.process}", flush=True)
 
     def infer(self, inputs):
